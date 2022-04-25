19991b2314fcef3204e911b83c7cd326e424cd02
Tamer Durgun <tmr.durgun@gmail.com>
2022-04-25 18:26:05 +0300

    themeprovider for styled components added

--- components/page/home/index.tsx
+++ components/page/home/index.tsx
@@ -1,12 +1,15 @@
 import User from "@domain/user";
 import { Wrapper } from './styled';
+import Theme from '@theme';
 
 const Home = () => {
   return (
     <>
+    <Theme>
       <Wrapper>HOME PAGE COMPONEN!
         <User/>
       </Wrapper>
+      </Theme>
     </>
   );
 };
--- components/page/home/styled.ts
+++ components/page/home/styled.ts
@@ -2,10 +2,11 @@ import styled from "styled-components";
 export const Title = styled.div`
   font-size: 1.5em;
   text-align: center;
-  color: ${(props) => props.color || "black"};
+  color: ${(props) => props.theme.colors.lightBlue || "black"};
 `;
 
 export const Wrapper = styled.div`
   padding: 4em;
   background: smokewhite;
+  color: ${(props) => props.theme.colors.persianGreen || "black"};
 `;
